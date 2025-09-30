543c2206f3ca4473b12a8b9326685f6b8477fe61
Anuoluwapo Gabriel Oyetibo <54692909+Aoyetibo557@users.noreply.github.com>
2025-09-30 12:56:20 -0400

    update: dist folder added

---
+++ dist/index.js
@@ -0,0 +1,12 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.default = handler;
+const app_1 = __importDefault(require("../app"));
+function handler(req, res) {
+    // Adapt Express app to Vercel serverless function
+    (0, app_1.default)(req, res);
+}
+//# sourceMappingURL=index.js.map
