e8fd27ee0e93863441b9a1346348701acacdfe4f
Anuoluwapo Gabriel Oyetibo <54692909+Aoyetibo557@users.noreply.github.com>
2025-09-30 11:23:38 -0400

    Fixing some build issues and errors and updating scripts to inclue build and prebuild commands

--- config/db.ts
+++ config/db.ts
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 import { SUPABASE_URL, SUPABASE_KEY } from "./index";
-import { Database } from "../database.types";
+import { Database } from "../types/supabase";
 
 //TODO: Update the database gen file when done and use it as type for the supabase client <Database>
 export const supabase = createClient(SUPABASE_URL!, SUPABASE_KEY!);
